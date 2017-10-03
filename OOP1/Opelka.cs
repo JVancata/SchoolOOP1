f3feb33f0582591ab7c69c0bed07923eef3d1188
Jakub Vančata <vancata.kuba@seznam.cz>
2017-10-03 10:14:23 +0200

    Training

---
+++ OOP1/Opelka.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Opelka
+    {
+        private string jmeno;
+        private string prijmeni;
+        private bool hasDs = false;
+        private string oblibenaOkurka;
+        private List<string> jmenaPsu;
+
+        //konstruktor
+        /*public Opelka()
+        {
+        }*/
+        public Opelka()
+        {
+        }
+        public Opelka(string jmeno = "Opelka", string prijmeni = "Opelka")
+        {
+            this.Jmeno = jmeno;
+            this.Prijmeni = prijmeni;
+        }
+
+        public override string ToString()
+        {
+            return "Právě vypisuješ třídu Opelka a opelku, který se jmenuje " + jmeno;
+        }
+        public string Jmeno { get; set; }
+
+        public string Prijmeni
+        {
+            get
+            {
+                return prijmeni;
+            }
+            set
+            {
+                prijmeni = value;
+            }
+        }
+
+        public string OblibenaOkurka { get; set; }
+
+        public List<string> JmenaPsu { get; set; }
+
+    }
+}
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -10,6 +10,53 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
+            Opelka opelka = new Opelka("Opelka", "Popelka");
+            //Opelka opelka = new Opelka();
+            //Console.WriteLine(opelka.Jmeno);
+            //opelka.Jmeno = "Opelka";
+            Console.Write("Zadej jméno svého opelky: ");
+            opelka.Jmeno = Console.ReadLine();
+
+            Console.Write("\nZadej příjmení svého opelky: ");
+            Console.ReadLine();
+            Console.WriteLine("\nSajkkkk, opelkové nemaj příjmení, hlupáku.\n");
+
+            Console.WriteLine("Ahoj {0}. Jak se máš? \n{0}: Já se mám skvěle. A taky smrdím. \nJaké je tvé jméno? \n{0}: No přece {0} \nA křestní? \n{0}: Nemám, hlupáku.", opelka.Jmeno);
+
+            Console.Write("\nZadej jméno jeho oblíbené okurky: ");
+            opelka.OblibenaOkurka = Console.ReadLine();
+
+            Console.WriteLine("\nAaaah, taky mám rád {0}", opelka.OblibenaOkurka);
+
+            Console.Write("\nMá {0} co má rád {1} psa?: ",opelka.Jmeno, opelka.OblibenaOkurka);
+            string hasPes = Console.ReadLine().ToLower();
+            //hasPes = hasPes.ToLower();
+            List<String> moznostiNaAno = new List<string>()
+            {
+                "jo", "ano", "jj", "j", "ne asi", "ma", "mam", "jasne", "neasi", "ma vole", "dva", "yes", "jes", "yez", "jez", "joo", "jooo", "jsem pes", "jo to jsem já", "jsem pes", "haf", "štěk", "stek", "jo má", "ty vole brachu jasná páka"
+            };
+            if (moznostiNaAno.Contains(hasPes))
+	        {
+                Console.WriteLine("\nNajs");
+                Console.WriteLine("Tak teď ho hezky zapíšeš, je to jasný?");
+                Console.WriteLine("To nebyla otázka, lol, zapíšeš");
+                Console.Write("\n Zapiš: ");
+            }
+            else
+            {
+                Console.WriteLine("\nTak to je blby, kup mu psa");
+            }
+            Console.WriteLine("Tak teď ho hezky zapíšeš, je to jasný?");
+
+            /*if (hasPes == "jo" || hasPes == "ano" || hasPes == "jj" || hasPes == "j" || hasPes == "j" || hasPes == "ne asi" || hasPes == "ma" || hasPes == "mam")
+            {
+
+            }
+            else
+            {
+                Console
+            }*/
+
         }
     }
 }
